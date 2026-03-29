696ab43792e1de14a8e0edff9724f3c79268b4df
Vedus OC <vedus-oc@noreply.github.com>
2026-03-30 07:50:43 +0900

    Use StrictMode and better logging

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+console.log('Mounting React...');
+
 const container = document.getElementById('root');
 if (container) {
-  const root = ReactDOM.createRoot(container);
-  root.render(<App />);
+  ReactDOM.createRoot(container).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log('Mounted');
 } else {
-  console.log('No root!');
+  console.error('No root!');
 }
