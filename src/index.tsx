737de80c562399c24f62dd2e5804fddad9cd3f6a
Vedus OC <vedus-oc@noreply.github.com>
2026-03-30 07:49:15 +0900

    Fix mounting

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = createRoot(document.getElementById('root')!);
-root.render(<App />);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
 EOF
