cc774c8c352224a62f4e3b9beb3607eb5e8c1fbd
Vedus OC <vedus-oc@noreply.github.com>
2026-03-30 07:48:12 +0900

    Simplify index mounting

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-console.log('Mounting React...');
+console.log('App starting');
 
-const container = document.getElementById('root');
-if (container) {
-  const root = createRoot(container);
-  console.log('Root found:', container);
-  root.render(<App />);
-  console.log('Render triggered');
-} else {
-  console.error('Root container not found!');
-}
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
+
+console.log('App rendered');
